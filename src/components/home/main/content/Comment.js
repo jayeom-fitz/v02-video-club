d193a2a3ab4d5ce590905438286b7e19da5bf150
jayeom-fitz <djawjdgus93@naver.com>
2021-07-20 01:23:22 +0900

    #1-13 Comment Func Part 1 - Frame

--- src/components/home/main/content/Comment.js
+++ src/components/home/main/content/Comment.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useHistory  } from "react-router-dom";
 
 import styled from 'styled-components'
 
@@ -6,7 +7,12 @@ import User from 'components/effect/User'
 import { getImageLinkByIdAndPlatform } from 'components/effect/function/func_video'
 import { lineFeedDecoding } from 'components/effect/function/func_str'
 
+import { GoThumbsup } from 'react-icons/go'
+import { ImBubble } from 'react-icons/im'
+import { RiExternalLinkLine } from 'react-icons/ri'
+
 function Comment(props) {
+  const history = useHistory();
 
   function imageClick() {
     props.setVideo({
@@ -45,6 +51,41 @@ function Comment(props) {
           </div>
         </Box>
 
+      </Content>
+
+      <Content style={{borderTop:'1px solid lightgrey'}}>     
+        <Button>
+          <ButtonBox>
+            <ButtonContent>
+              <ButtonIcon>
+                <GoThumbsup size='20' color='grey' style={{verticalAlign: 'middle', paddingBottom: '5px'}}/>
+              </ButtonIcon>
+              <ButtonText> 추천 </ButtonText>
+            </ButtonContent>
+          </ButtonBox>
+        </Button>
+
+        <Button>
+          <ButtonBox>
+            <ButtonContent>
+              <ButtonIcon>
+                <ImBubble size='20' color='grey' style={{verticalAlign: 'middle', paddingBottom: '5px'}}/>
+              </ButtonIcon>
+              <ButtonText> 댓글 </ButtonText>
+            </ButtonContent>
+          </ButtonBox>
+        </Button>
+
+        <Button>
+          <ButtonBox>
+            <ButtonContent>
+              <ButtonIcon>
+                <RiExternalLinkLine size='20' color='grey' style={{verticalAlign: 'middle', paddingBottom: '5px'}}/>
+              </ButtonIcon>
+              <ButtonText> 링크 </ButtonText>
+            </ButtonContent>
+          </ButtonBox>
+        </Button>
       </Content>
     </Container>
   )
@@ -86,50 +127,27 @@ const Pre = styled.pre`
   line-height: 150%;
   white-space: pre-wrap;
 `
-
-const InputBox = styled.div`
-  width: 100%;
-  text-align: center;
+const Button = styled.div`
+  flex: 1;
+  height: 40px;
+  line-height: 40px;
 `
-const Input = styled.input`
-  width: 80%;
-  height: 30px;
-  margin: 5px auto;
-  padding: 0 10px;
-  border: none;
-  border-bottom: 1px solid grey;
-
-  &:focus {
-    outline: none;
-  }
+const ButtonBox = styled.div`
+  width: 40%;
+  margin: auto;
 `
-const Textarea = styled.textarea`
-  width: 80%;
-  height: 150px;
-  resize: none;
-  padding: 10px;
-  margin: 5px auto;
-  border: 1px solid grey;
-
-  &:focus {
-    outline: none;
-  }
+const ButtonContent = styled.div`
+  display: flex;
+`
+const ButtonIcon = styled.div`
+  height: 100%;
+`
+const ButtonText = styled.div`
+  color: grey;
+  font-weight: 800;
+  padding-left: 10px;
 `
 
 
-const Button = styled.button`
-  width: 80%;
-  height: 30px;
-  margin: 5px auto;
-  border: none;
-  color: white;
-  font-size: 14px;
-  font-weight: 700;
-  background-color: #f70d1a;
-
-  &:hover {
-    background-color: #ff3d3a;
-  }
-`
 // #f70d1a ferrari red
 // #ffa6c9 carnation
