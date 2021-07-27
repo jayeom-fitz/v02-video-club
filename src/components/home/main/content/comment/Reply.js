b87568174b495c76011209265345bf147fe88a27
jayeom-fitz <djawjdgus93@naver.com>
2021-07-27 23:38:53 +0900

    #1-20 Comment Reply Part 5 - Edit

--- src/components/home/main/content/comment/Reply.js
+++ src/components/home/main/content/comment/Reply.js
@@ -12,20 +12,21 @@ import Avatar from "@material-ui/core/Avatar";
 import { FiEdit2 } from 'react-icons/fi'
 import { ImCross } from 'react-icons/im'
 import { GoThumbsup } from 'react-icons/go'
-import { replyRecommend } from 'fb/reply/set';
+import { replyRecommend, updateReplyContent } from 'fb/reply/set';
+import { lineFeedEncoding } from 'components/effect/function/func_str';
 
 function Reply(props) {
   const [reply, setReply] = useState(props.reply)
-  
-  const [loaded, setLoaded] = useState(false);
   const [recommend, setRecommend] = useState(false);
+
+  const [edit, setEdit] = useState(false);
+  const [content, setContent] = useState(props.reply.content);
 
   async function checkRecommend() {
     if(!(props.user === null || props.user === undefined)) {
       const data = await isClikedLike(reply.id, props.user.uid);
       setRecommend(data);
     }    
-     setLoaded(true);
   }
 
   useEffect(() => {
@@ -40,13 +41,28 @@ function Reply(props) {
     setReply(data); setRecommend(true);
   }
 
+  function onChange(t) {
+    t.style.height = '1px';
+    t.style.height = (t.scrollHeight) + 'px';
+    setContent(t.value);
+  }
+
+  async function onKeyDown(e) {
+    if(e.code === 'Enter') { 
+      await updateReplyContent(reply.id, lineFeedEncoding(content));
+      var r = reply; r.content = content; 
+      alert('댓글이 수정되었습니다.')
+      setReply(r);  setEdit(false);
+    } 
+  }
+
   return (
     <Container>
       <div>
         <StyledAvatar src={reply.pimage}/>
       </div>
 
-      <div style={{paddingLeft:'10px'}}>
+      <div style={{paddingLeft:'10px', width:'80%'}}>
         <div style={{display:'flex'}}>
           <Name>
             <StyledLink to={`/user/${reply.pid}`}>
@@ -56,7 +72,7 @@ function Reply(props) {
                  
           {props.user && (props.user.uid === props.reply.pid || props.user.level >= 1) &&
           <div style={{verticalAlign:'middle'}}>
-            <StyledFiEdit size='12' />
+            <StyledFiEdit size='12' onClick={() => setEdit(!edit)} />
           </div>}
 
           {props.user && (props.user.uid === props.reply.pid || props.user.level >= 1) &&
@@ -79,10 +95,16 @@ function Reply(props) {
           
         </div>
 
-        <div>
+        <div style={{width:'100%'}}>
+          {edit ? 
+          <Input value={content} 
+                onChange={(e) => onChange(e.target) } 
+                onKeyDown={(e) => onKeyDown(e)}/>
+          :
           <Content>
             {reply.content}
           </Content>
+          }
         </div>
       </div>  
     </Container>
@@ -143,4 +165,19 @@ const UpCount = styled.span`
   color: #f70d1a;
   padding-right: 10px;
   font-size: 0.9rem;
+`
+const Input = styled.textarea`
+  width: 100%;
+  min-height: 0.8rem;
+  overflow-y: hidden;
+  margin: 10px;
+  padding: 10px 10px 0 10px;
+  resize: none;
+  border: none;
+  border-radius: 15px;
+  background-color: #ffa6c9;
+
+  &:focus {
+    outline: none;
+  }
 `
